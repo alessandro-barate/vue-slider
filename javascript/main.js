c2d718aaf286c1cc4ccaa8c4864e31bc554654ee
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-21 16:02:25 +0200

    [UPD] Main images added

--- javascript/main.js
+++ javascript/main.js
@@ -1,86 +1,79 @@
 "use strict";
 
-// Array
-const images = [
-  {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morales",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-  },
-  {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-  },
-  {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  },
-  {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-  },
-  {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
-
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      images: [
+        "./img/01.webp",
+        "./img/02.webp",
+        "./img/03.webp",
+        "./img/04.webp",
+        "./img/05.webp",
+      ],
+
+      titles: [
+        "Marvel's Spiderman Miles Morales",
+        "Ratchet & Clank: Rift Apart",
+        "Fortnite",
+        "Stray",
+        "Marvel's Avengers",
+      ],
+
+      texts: [
+        "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+        "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+        "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+        "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+      ],
+
+      // Variabile contatore
+      active: 0,
     };
   },
 }).mount("#app");
 
-// Variabile contatore
-let active = 0;
+// // Variabile per il setInterval
+// let interval;
 
-// Variabile per il setInterval
-let interval;
+// const items = document.querySelector(".items");
+// const thumbs = document.querySelector(".thumbs");
 
-const items = document.querySelector(".items");
-const thumbs = document.querySelector(".thumbs");
+// // Ciclo sull'array per creare gli elementi HTML
+// for (let i = 0; i < images.length; i++) {
+//   const imagesArray = images[i];
+//   const div1 = document.createElement("div");
+//   const div2 = document.createElement("div");
+//   div1.classList.add("item");
+//   div2.classList.add("thumb");
+//   div2.addEventListener("click", function () {
+//     document
+//       .getElementsByClassName("thumb", "active")
+//       [active].classList.remove("active");
 
-// Ciclo sull'array per creare gli elementi HTML
-for (let i = 0; i < images.length; i++) {
-  const imagesArray = images[i];
-  const div1 = document.createElement("div");
-  const div2 = document.createElement("div");
-  div1.classList.add("item");
-  div2.classList.add("thumb");
-  div2.addEventListener("click", function () {
-    document
-      .getElementsByClassName("thumb", "active")
-      [active].classList.remove("active");
+//     next(i);
+//     document.getElementsByClassName("thumb")[active].classList.add("active");
+//   });
 
-    next(i);
-    document.getElementsByClassName("thumb")[active].classList.add("active");
-  });
+//   const h2 = document.createElement("h2");
+//   h2.innerText = imagesArray.title;
+//   h2.classList.add("positioning", "h2", "bg");
 
-  const h2 = document.createElement("h2");
-  h2.innerText = imagesArray.title;
-  h2.classList.add("positioning", "h2", "bg");
+//   const p = document.createElement("p");
+//   p.innerText = imagesArray.text;
+//   p.classList.add("positioning", "p", "bg");
 
-  const p = document.createElement("p");
-  p.innerText = imagesArray.text;
-  p.classList.add("positioning", "p", "bg");
-
-  const img1 = document.createElement("img");
-  const img2 = document.createElement("img");
-  img1.src = "../../" + imagesArray.image;
-  img2.src = "../../" + imagesArray.image;
-  div1.append(img1, h2, p);
-  div2.append(img2);
-  items.append(div1);
-  thumbs.append(div2);
-}
+//   const img1 = document.createElement("img");
+//   const img2 = document.createElement("img");
+//   img1.src = "../../" + imagesArray.image;
+//   img2.src = "../../" + imagesArray.image;
+//   div1.append(img1, h2, p);
+//   div2.append(img2);
+//   items.append(div1);
+//   thumbs.append(div2);
+// }
 
 // Funzione per settare l'immagine iniziale ed il setInterval
 function firstImg() {
